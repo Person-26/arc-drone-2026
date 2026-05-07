5d9471aac6646e3dc99557909884187cbe95075d
Charles Coldwell <coldwell@gmail.com>
2026-05-06 20:04:57 -0400

    RTSP testbench

---
+++ rtsp.cpp
@@ -0,0 +1,31 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+	cv::VideoCapture cap("rtsp://tocatta:8554/cam?tcp", cv::CAP_FFMPEG);
+	cap.set(cv::CAP_PROP_BUFFERSIZE, 1);
+
+	if (!cap.isOpened()) {
+		std::cerr << "Failed to open stream" << std::endl;
+		return -1;
+	}
+
+	cv::Mat frame;
+	while (true) {
+		cap >> frame;
+		if (frame.empty()) break;
+
+		cv::imshow("frame", frame);
+		if ((cv::waitKey(1) & 0xFF) == 'q') break;
+	}
+
+	cap.release();
+	cv::destroyAllWindows();
+	return 0;
+}
+
+// Local variables:
+// c-basic-offset: 4
+// tab-width: 4
+// indent-tabs-mode: t
+// End:
